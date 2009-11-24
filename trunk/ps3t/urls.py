e5ca5f77881770870b50183c414500a947b73a17
linux.fabricio <linux.fabricio@6bcd71d2-7154-11de-b0c7-2b5f3fb92cbb>
2009-11-24 19:26:31 +0000

    Layout da pagina de usuário praticamento finalizada  \o

--- trunk/ps3t/urls.py
+++ trunk/ps3t/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf.urls.defaults import *
+from ps3t import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,3 +19,9 @@ urlpatterns = patterns('',
     (r'^rank/$', 'ps3t.myps3t.views.rank'),
     (r'^rank/(?P<userName>.{1,})/$', 'ps3t.myps3t.views.rankUser'),
 )
+
+if settings.DEBUG:
+	urlpatterns += patterns('',
+		(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.IMG_ROOT, 'show_indexes': True}),
+    )
+
