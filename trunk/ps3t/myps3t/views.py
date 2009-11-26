8aa2723eecbd6e31a1604294e3278c80a5486e97
linux.fabricio <linux.fabricio@6bcd71d2-7154-11de-b0c7-2b5f3fb92cbb>
2009-11-26 17:27:28 +0000

    1 - Mexi nas tabelas de usuários e jogos de usuários, as 2 estão finalizadas. 3 - Transportei os CSS para arquivos separados 4 - Troquei o dir "img" por "media" para ficar mais generico ( tive que mudar configuração por conta disso") 5 - PSN.PY - baixei o perfil e jogos de 5 usuarios, e ele esta rodando em cima deles 6 - Deve ter mais coisa q eu esqueci =D

--- trunk/ps3t/myps3t/views.py
+++ trunk/ps3t/myps3t/views.py
@@ -4,18 +4,35 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import get_list_or_404
 from django.shortcuts import render_to_response
 import ps3t.myps3t.models as db
+import pprint
+
+def user_compare(user_a, user_b):
+	return user_a[2] - user_b[2]
+	
 
 def rank(request):
+	
+	BRONZE_VALUE   = 15
+	SILVER_VALUE   = 30
+	GOLD_VALUE     = 90
+	PLATINUM_VALUE = 180
 
 	users  = get_list_or_404(db.userTrophy.objects.all())
 
 	perc_list = []
 
 	for user in users:
-		print user.user.psn_id
-		print user.user.pic_url
 		perc =  (( 202 * user.perc_level) / 100)
-		perc_list.append([user, perc])
+		points_total  = user.platinum + user.gold + user.silver + user.bronze
+
+		points_total = ( user.platinum * PLATINUM_VALUE
+							+ user.gold     * GOLD_VALUE
+							+ user.silver   * SILVER_VALUE 
+							+ user.bronze   * BRONZE_VALUE)
+
+		perc_list.append([user, perc, points_total])
+
+	perc_list.sort(user_compare, reverse=True)
 
 	return render_to_response('Rank.html',
 				{'users' : perc_list })
@@ -30,7 +47,13 @@ def rankUser(request, userName):
 	for gameInfo in user_game_info:
 		perc =  (( 202 * gameInfo.perc_done ) / 100)
 		trophy_total = gameInfo.platinum + gameInfo.gold + gameInfo.silver + gameInfo.bronze
-		perc_list.append([gameInfo, perc, trophy_total])
+
+		if gameInfo.platinum is 0 and gameInfo.perc_done is 100:
+			type = 1
+		else:
+			type = 0
+				
+		perc_list.append([gameInfo, perc, trophy_total, type])
 
 	return render_to_response('userRank.html',
 				{'user'          : user,
